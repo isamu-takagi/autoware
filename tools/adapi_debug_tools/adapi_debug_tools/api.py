d3ceec6d4f438d39c4f7caee297d298fa2ace3d0
Takagi, Isamu <isamu.takagi@tier4.jp>
2022-11-08 09:42:19 +0900

    feat: move adapi debug
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- tools/adapi_debug_tools/adapi_debug_tools/api.py
+++
@@ -1,7 +0,0 @@
-import rclpy.node
-
-
-class ApiNode(rclpy.node.Node):
-
-    def __init__(self, node_name):
-        super().__init__(node_name)
--- tools/adapi_debug_tools/adapi_debug_tools/widgets/__init__.py
+++
@@ -1,1 +0,0 @@
-from .operation_mode import OperationModeApiWidget
