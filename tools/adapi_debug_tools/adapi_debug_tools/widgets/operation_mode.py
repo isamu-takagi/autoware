d4a8cc7030470935332d4958f4b8e9ce6d071bf8
Takagi, Isamu <isamu.takagi@tier4.jp>
2022-11-10 20:46:55 +0900

    feat: update debug tool
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- tools/adapi_debug_tools/adapi_debug_tools/widgets/operation_mode.py
+++ tools/adapi_debug_tools/adapi_debug_tools/widgets/operation_mode.py
@@ -1,16 +1,10 @@
-from python_qt_binding import QtWidgets
+from functools import partial
+from python_qt_binding import QtCore, QtWidgets
 
 from adapi_debug_tools.api import Adapi
 from autoware_adapi_v1_msgs.msg import OperationModeState
 from autoware_adapi_v1_msgs.srv import ChangeOperationMode
 
-mode_text = {
-    OperationModeState.UNKNOWN: "UNKNOWN",
-    OperationModeState.STOP: "STOP",
-    OperationModeState.AUTONOMOUS: "AUTONOMOUS",
-    OperationModeState.LOCAL: "LOCAL",
-    OperationModeState.REMOTE: "REMOTE",
-}
 
 class OperationModeApiWidget(QtWidgets.QWidget):
 
@@ -20,66 +14,70 @@ class OperationModeApiWidget(QtWidgets.QWidget):
         self.sub_state = adapi.operation_mode.state.subscribe(self.on_state)
         self.cli_mode = adapi.operation_mode.change_mode
 
-        self.operation_mode = QtWidgets.QLabel("UNKNOWN")
-        self.autoware_control = QtWidgets.QLabel("UNKNOWN")
-        self.transition = QtWidgets.QLabel("UNKNOWN")
-        self.button_stop_mode = QtWidgets.QPushButton("STOP")
-        self.button_auto_mode = QtWidgets.QPushButton("AUTO")
-        self.button_local_mode = QtWidgets.QPushButton("LOCAL")
-        self.button_remote_mode = QtWidgets.QPushButton("REMOTE")
-        self.button_enable = QtWidgets.QPushButton("enable")
-        self.button_disable = QtWidgets.QPushButton("disable")
+        self.buttons = {}
+        self.mode_label = QtWidgets.QLabel("unknown")
+        self.mode_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.mode_label.setStyleSheet("QLabel { border: 1px solid; padding: 2px; }")
 
-        style = '* [status="enable"] { background-color: lime; }'
-        self.button_stop_mode.setStyleSheet(style)
-        self.button_auto_mode.setStyleSheet(style)
-        self.button_local_mode.setStyleSheet(style)
-        self.button_remote_mode.setStyleSheet(style)
-        self.button_enable.setStyleSheet(style)
-        self.button_disable.setStyleSheet(style)
+        button_settings = [
+            ("enable", adapi.operation_mode.enable_control),
+            ("disable", adapi.operation_mode.disable_control),
+            ("stop", adapi.operation_mode.change_stop),
+            ("auto", adapi.operation_mode.change_auto),
+            ("local", adapi.operation_mode.change_local),
+            ("remote", adapi.operation_mode.change_remote),
+        ]
 
-        self.button_stop_mode.clicked.connect(lambda: self.change_mode(adapi.operation_mode.change_stop))
-        self.button_auto_mode.clicked.connect(lambda: self.change_mode(adapi.operation_mode.change_auto))
-        self.button_local_mode.clicked.connect(lambda: self.change_mode(adapi.operation_mode.change_local))
-        self.button_remote_mode.clicked.connect(lambda: self.change_mode(adapi.operation_mode.change_remote))
-        self.button_enable.clicked.connect(lambda: self.change_mode(adapi.operation_mode.enable_control))
-        self.button_disable.clicked.connect(lambda: self.change_mode(adapi.operation_mode.disable_control))
+        button_style = 'QPushButton[status="enable"] { background-color: lime; }'
+        for name, api in button_settings:
+            button = QtWidgets.QPushButton(name)
+            button.setStyleSheet(button_style)
+            button.clicked.connect(partial(self.change_mode, api))
+            self.buttons[name] = button
 
         mode_layout = QtWidgets.QHBoxLayout()
-        mode_layout.addWidget(self.button_stop_mode)
-        mode_layout.addWidget(self.button_auto_mode)
-        mode_layout.addWidget(self.button_local_mode)
-        mode_layout.addWidget(self.button_remote_mode)
+        for name in ("stop", "auto", "local", "remote"):
+            mode_layout.addWidget(self.buttons[name])
 
-        control_layout = QtWidgets.QHBoxLayout()
-        control_layout.addWidget(self.button_enable)
-        control_layout.addWidget(self.button_disable)
+        ctrl_layout = QtWidgets.QHBoxLayout()
+        for name in ("enable", "disable"):
+            ctrl_layout.addWidget(self.buttons[name])
 
         layout = QtWidgets.QGridLayout()
-        layout.addWidget(QtWidgets.QLabel("operation mode"), 0, 0)
-        layout.addWidget(QtWidgets.QLabel("autoware control"), 1, 0)
-        layout.addWidget(QtWidgets.QLabel("transition"), 2, 0)
-        layout.addWidget(self.operation_mode, 0, 1)
-        layout.addWidget(self.autoware_control, 1, 1)
-        layout.addWidget(self.transition, 2, 1)
-        layout.addLayout(mode_layout, 0, 2)
-        layout.addLayout(control_layout, 1, 2)
+        layout.addWidget(QtWidgets.QLabel("operation mode state"), 0, 0)
+        layout.addWidget(QtWidgets.QLabel("change operation mode"), 1, 0)
+        layout.addWidget(QtWidgets.QLabel("change autoware control"), 2, 0)
+        layout.addWidget(self.mode_label, 0, 1)
+        layout.addLayout(mode_layout, 1, 1)
+        layout.addLayout(ctrl_layout, 2, 1)
         layout.setRowStretch(3, 1)
         self.setLayout(layout)
-
 
     def change_mode(self, client):
         client.call_async(ChangeOperationMode.Request())
 
     def on_state(self, msg):
-        self.operation_mode.setText(mode_text[msg.mode])
-        self.autoware_control.setText(str(msg.is_autoware_control_enabled))
-        self.transition.setText(str(msg.is_in_transition))
-        self.button_stop_mode.setProperty("status", "enable" if msg.is_stop_mode_available else "disable")
-        self.button_auto_mode.setProperty("status", "enable" if msg.is_autonomous_mode_available else "disable")
-        self.button_local_mode.setProperty("status", "enable" if msg.is_local_mode_available else "disable")
-        self.button_remote_mode.setProperty("status", "enable" if msg.is_remote_mode_available else "disable")
+        change_availables = {
+            "stop": msg .is_stop_mode_available,
+            "auto": msg.is_autonomous_mode_available,
+            "local": msg.is_local_mode_available,
+            "remote": msg.is_remote_mode_available,
+        }
+        for name in ("stop", "auto", "local", "remote"):
+            button = self.buttons[name]
+            status = "enable" if change_availables[name] else "disable"
+            if button.property("status") != status:
+                button.setProperty("status", status)
+                button.style().polish(button)
 
-        #for button in (self.button_stop_mode, self.button_auto_mode, self.button_local_mode, self.button_remote_mode):
-        #    button.style().unpolish(button)
-        #    button.style().polish(button)
+        mode_text = {
+            OperationModeState.UNKNOWN: "unknown",
+            OperationModeState.STOP: "stop",
+            OperationModeState.AUTONOMOUS: "auto",
+            OperationModeState.LOCAL: "local",
+            OperationModeState.REMOTE: "remote",
+        }
+        text1 = "transition to " if msg.is_in_transition else ""
+        text2 = mode_text[msg.mode]
+        text3 = "autoware" if msg.is_autoware_control_enabled else "manual"
+        self.mode_label.setText(f"{text1} {text2} ({text3} control)")
