e45714df94b41a254fc1fe3e0ec0ff4549af6dc7
Takagi, Isamu <isamu.takagi@tier4.jp>
2022-12-07 21:41:36 +0900

    WIP
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- tools/adapi_debug_tools/adapi_debug_tools/widgets/settings.py
+++ tools/adapi_debug_tools/adapi_debug_tools/widgets/settings.py
@@ -15,6 +15,21 @@ class UpdateAndCancel(QtWidgets.QHBoxLayout):
         self.addWidget(self.cancel)
 
 
+class PoseInputLayout(QtWidgets.QGridLayout):
+
+    def __init__(self):
+        super().__init__()
+        self.fields = ["x", "y", "z", "roll", "pitch", "yaw"]
+        self.inputs = {field: QtWidgets.QLineEdit() for field in self.fields}
+
+        for index, field in enumerate(self.fields):
+            row = int((index * 2) / 6)
+            col = int((index * 2) % 6)
+            self.addWidget(QtWidgets.QLabel(field), row, col)
+            self.addWidget(self.inputs[field], row, col + 1)
+
+
+
 class PoseCovDialog(QtWidgets.QDialog):
 
     def __init__(self, adapi: Adapi, parent: QtWidgets.QWidget):
@@ -23,8 +38,10 @@ class PoseCovDialog(QtWidgets.QDialog):
         self.footer = UpdateAndCancel(self)
         self.editor = QtWidgets.QTextEdit()
         self.editor.setPlainText(adapi.settings.get_yaml("initial-pose"))
+        self.pose = PoseInputLayout()
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(self.editor)
+        layout.addLayout(self.pose)
         layout.addLayout(self.footer)
         self.setLayout(layout)
 
