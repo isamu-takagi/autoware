40ae96174157e745a4d4c24108d9da239c2fb3f5
Takagi, Isamu <isamu.takagi@tier4.jp>
2023-07-07 16:35:52 +0900

    add component interface tool
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

---
+++ tools/interfaces/component.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from pathlib import Path
+import yaml
+
+components = frozenset([
+    "map",
+    "localization",
+    "sensing",
+    "perception",
+    "planning",
+    "control",
+    "system",
+    "simulation",
+    "adapi",
+    "tier4_api",
+    "rviz",
+])
+
+mapping = {
+    "pose_initializer_node": "localization",
+    "robot_state_publisher": "sensing",
+    "occupancy_grid_map": "perception",
+    "aggregator_node": "system",
+    "static_map_to_odom_tf_publisher": "simulation",
+    "default_ad_api": "adapi",
+    "autoware_api": "tier4_api",
+    "rviz2": "rviz",
+}
+
+
+def classify_component(name):
+    component = name.split("/")[1]
+    component = mapping.get(component, component)
+    if component not in components:
+        raise RuntimeError("unknown component: " + component)
+    return component
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("interfaces")
+    args = parser.parse_args()
+
+    interfaces = yaml.safe_load(Path(args.interfaces).read_text())["links"]
+    for interface in interfaces:
+        print(interface["name"])
+        for node_type, node_name in interface["uses"]:
+            if node_name.startswith("/transform_listener_impl_"):
+                continue
+            component = classify_component(node_name)
+            print(" -", node_type, component, node_name)
+
+if __name__ == "__main__":
+    main()
