048d3114c0a788fe9c4f22c1c7659fc10b79e0ea
Takagi, Isamu <isamu.takagi@tier4.jp>
2023-07-07 14:55:29 +0900

    update interface print
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- tools/interfaces/print.py
+++ tools/interfaces/print.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+import collections
+import os
 import rclpy
 import yaml
+
 
 class WaitTimer:
 
@@ -18,16 +21,73 @@ class WaitTimer:
         self.node.destroy_timer(timer)
 
 
+class PortInfo:
+
+    builtins = ["rcl_interfaces", "composition_interfaces"]
+
+    def __init__(self, type, name, msgs):
+        self.type = type
+        self.name = name
+        self.msgs = msgs
+
+    def is_builtin_interface(self):
+        for msg in self.msgs:
+            for pkg in self.builtins:
+                if msg.startswith(pkg + "/"):
+                    return True
+        return False
+
+    def dump(self):
+        return [self.type, self.name, *self.msgs]
+
+
+class LinkInfo:
+
+    def __init__(self):
+        self.name = None
+        self.msgs = set()
+        self.uses = []
+
+    def register_node(self, node, port):
+        self.msgs |= set(port.msgs)
+        self.uses.append([port.type, node.full_name])
+
+    def dump(self):
+        return {"name": self.name, "msgs": list(self.msgs), "uses": self.uses}
+
+
+class NodeInfo:
+
+    def __init__(self, node_name, namespace):
+        self.node_name = node_name
+        self.namespace = namespace
+        self.interface = []
+
+    @property
+    def full_name(self):
+        return os.path.join(self.namespace, self.node_name)
+
+    def register_interface(self, type, name_and_msgs):
+        interface = [PortInfo(type, name, msgs) for name, msgs in name_and_msgs]
+        interface = [port for port in interface if not port.is_builtin_interface()]
+        self.interface.extend(interface)
+
+    def dump(self):
+        return {"full_name": self.full_name, "interface": [port.dump() for port in self.interface]}
+
+
 class SaveGraph:
 
     def __init__(self, node):
         self.node = node
 
     def print_graph_info(self):
+        nodes = self.collect_node_info(self.node)
+        links = self.collect_link_info(nodes)
         graph = dict()
-        graph["nodes"] = self.collect_node_info(self.node)
-        graph["links"] = self.collect_link_info(self.node)
-        print(yaml.safe_dump(graph, default_flow_style=None))
+        graph["nodes"] = [info.dump() for info in nodes]
+        graph["links"] = [info.dump() for info in links]
+        print(yaml.safe_dump(graph, default_flow_style=None, width=200))
 
     @classmethod
     def compress_name_and_types(cls, name_and_types):
@@ -36,22 +96,24 @@ class SaveGraph:
     @classmethod
     def collect_node_info(cls, node):
         nodes = list()
-        for names in node.get_node_names_and_namespaces():
-            info = dict()
-            info["name"] = names
-            info["subs"] = cls.compress_name_and_types(node.get_subscriber_names_and_types_by_node(*names))
-            info["pubs"] = cls.compress_name_and_types(node.get_publisher_names_and_types_by_node(*names))
-            info["clis"] = cls.compress_name_and_types(node.get_client_names_and_types_by_node(*names))
-            info["srvs"] = cls.compress_name_and_types(node.get_service_names_and_types_by_node(*names))
+        for node_name, namespace in node.get_node_names_and_namespaces():
+            info = NodeInfo(node_name, namespace)
+            info.register_interface("sub", node.get_subscriber_names_and_types_by_node(info.node_name, info.namespace))
+            info.register_interface("pub", node.get_publisher_names_and_types_by_node(info.node_name, info.namespace))
+            info.register_interface("cli", node.get_client_names_and_types_by_node(info.node_name, info.namespace))
+            info.register_interface("srv", node.get_service_names_and_types_by_node(info.node_name, info.namespace))
             nodes.append(info)
         return nodes
 
     @classmethod
-    def collect_link_info(cls, node):
-        # node.get_topic_names_and_types()
-        # node.get_subscriptions_info_by_topic(name)
-        # node.get_publishers_info_by_topic(name)
-        return cls.compress_name_and_types(node.get_topic_names_and_types())
+    def collect_link_info(cls, nodes):
+        links = collections.defaultdict(LinkInfo)
+        for node in nodes:
+            for port in node.interface:
+                links[port.name].register_node(node, port)
+        for name, link in links.items():
+            link.name = name
+        return list(links.values())
 
 
 def main():
@@ -65,3 +127,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# node.get_topic_names_and_types()
+# node.get_subscriptions_info_by_topic(name)
+# node.get_publishers_info_by_topic(name)
